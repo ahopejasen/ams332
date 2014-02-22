a14ffa8087ace2dc251182d21534d149638ef100
A Hope Jasentuliyana <a.hope.jasen@gmail.com>
2014-02-22 11:52:23 -0500

    Added colored phase-plane plot. %todo flesh-out chart titles, add color legend to phase portrait

--- project1/autoregODE/autoReg.m
+++ project1/autoregODE/autoReg.m
@@ -60,20 +60,21 @@ startTime=0; % (s)
 endTime=20; % (s)
 timeStep=0.01; % (s) delta_t for approximation
 
+% result array index constants 
+i_rna=1;
+i_pro=2;
+
 %%initialize result arrays
-%my results
-%X=zeros(numVars,1,numRuns); % vars x time x runs
-%timeVector=zeros(1,numRuns); %  time x runs
-%matlab's ODE results
-%t_ode=zeros(1,numRuns);
-%y_ode=zeros(1,1,numRuns);
+clear('X','timeVector','t_ode','y_ode');
 
+%in case we want to change ode45() parameters:
+odeOptions=odeset();
+%odeOptions.RelTol=0.001;
+%odeOptions.AbsTol=0.001;
+%odeOptions.NonNegative=1;
 
-op=odeset();
-%op.RelTol=0.000001;
-%op.AbsTol=0.000001;
-%op.NonNegative=1;
-%% run the approximation algorithms, once for each set of IC's
+%% Project Part 1 & 2
+%run the approximation algorithms, once for each set of IC's
 for theRun=1:numRuns
 	X0=[r0(theRun);p0(theRun)]; % initial conditions
 
@@ -84,16 +85,16 @@ for theRun=1:numRuns
 		% NOTE: variable values (eg mu, k) are stored as constants
 		% in the funtion handle when it is declared, and persist when
 		% the handle is passed to another function
-	dr_dt=@(r,p) mu*p^2/(k^2 + p^2) - chi_r*r; % mRNA rate of change
-	dp_dt=@(r,p) omega*r - chi_p*p; % protein rate of change
+	dr_dt=@(r,p) mu.*p.^2./(k.^2 + p.^2) - chi_r.*r; % mRNA rate of change
+	dp_dt=@(r,p) omega.*r - chi_p.*p; % protein rate of change
 
 	dX_dt={dr_dt; dp_dt}; % this is a cell-array of function handles
 
 	[timeVector(:,theRun),X(:,:,theRun)]=doForwardEuler(dX_dt,X0,startTime,endTime,timeStep);
 
 	% compare with MatLab ODE45 (a runge-kutta method)
-	dy_dt=@(t,y) [mu *y(2)^2/(k^2 + y(2)^2) - chi_r * y(1);omega * y(1) - chi_p * y(2)]; 
-	[t_ode(:,theRun),y_ode(:,:,theRun)]=ode45(dy_dt,[startTime:0.5:endTime],X0,op);
+	dy_dt=@(t,y) [mu *y(i_pro)^2/(k^2 + y(i_pro)^2) - chi_r * y(i_rna);omega * y(i_rna) - chi_p * y(i_pro)]; 
+	[t_ode(:,theRun),y_ode(:,:,theRun)]=ode45(dy_dt,[startTime:0.5:endTime],X0,odeOptions);
 end %run
 
 
@@ -103,7 +104,7 @@ for theRun=1:numRuns
 	hold on;
 	plot(timeVector(:,theRun).',X(:,:,theRun.')); %plot() wants column vectors
 	%plot ode45 as crosses
-	plot(t_ode(:,theRun),y_ode(:,1,theRun),'b+',t_ode(:,theRun),y_ode(:,2,theRun),'g+');
+	plot(t_ode(:,theRun),y_ode(:,i_rna,theRun),'b+',t_ode(:,theRun),y_ode(:,i_pro,theRun),'g+');
 	legend({'mRNA','protein'},'Location','East');
 	% trick to do multiline title *with* variable values:
 	% http://mechatronics.me.wisc.edu/labresources/MatlabTipsNTricks.htm
@@ -111,3 +112,54 @@ for theRun=1:numRuns
 	xlabel('time (s)');
 	ylabel('concentration (mM)')
 end
+
+%%project part 3: phase plane
+
+clear('X','timeVector');
+icVector=[0.0:0.2:1.4];%initial concentrations in mM
+numRuns=size(icVector,2)^2;
+theRun=1; %index variable for X
+figure();
+hold on;
+for r0=icVector %initial RNA concentrations in mM
+	for p0=icVector %initial protein concentrations in mM
+		X0=[r0;p0];
+		[timeVector(:,theRun),X(:,:,theRun)]=doForwardEuler(dX_dt,[r0;p0],startTime,endTime,timeStep);
+		%plot results
+		plot(timeVector(:,theRun).',X(:,:,theRun).');		
+		theRun=theRun+1;
+	end
+end
+title({'Concentration vs time for autoregulatory model'; ...
+	['for ',num2str(numRuns),' combinations of initial conditions']});
+xlabel('time (s)');
+ylabel('concentration (mM)');
+
+
+%% phase plane plot
+%color showing slope direction
+%red: protein & rna increasing (NorthEast)
+%yellow: protein increasing, rna decreasing (SE)
+figure();
+hold on;
+for theRun=1:numRuns
+	XRNA=squeeze(X(i_rna,:,theRun)); %vector of RNA concentrations over time
+	XProt=squeeze(X(i_pro,:,theRun));  %vector of protein concentration ove time
+	dRNA=(dr_dt(X(i_rna,:,theRun),(X(i_pro,:,theRun)))); %vector of dRNA/dt
+	dProtein=(dp_dt(X(i_rna,:,theRun),(X(i_pro,:,theRun)))); %vector of dProtein/dt
+	dRNAUp=(dRNA>0); % logical array of positive RNA slopes
+	dProteinUp=(dProtein>0); %logical array of positive Protein slopes
+	d_rup_pup=dRNAUp & dProteinUp; %logical array of slopes to NE
+	d_rup_pdn=dRNAUp & ~ dProteinUp; % SE slopes
+	d_rdn_pup= ~ dRNAUp & dProteinUp; % NW slopes
+	d_rdn_pdn= ~ dRNAUp & ~ dProteinUp; % SW slopes
+	plot(XRNA(d_rup_pup).', XProt(d_rup_pup).',':r'); %phase plot of points with NE slope
+	plot(XRNA(d_rup_pdn).', XProt(d_rup_pdn).',':y'); %SE slopes
+	plot(XRNA(d_rdn_pup).', XProt(d_rdn_pup).',':m'); %NW
+	plot(XRNA(d_rdn_pdn).', XProt(d_rdn_pdn).',':b'); %SW
+end
+xlabel('mRNA (mM)');
+ylabel('protein (mM)');
+title('Phase portrait of autoregulatory simulation');
+%todo flesh-out chart titles, add color legend to phase portrait
+hold off;
